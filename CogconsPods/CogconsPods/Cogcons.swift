362a2d304541a11ad3a67b3cf4e52d181ebd5e94
rajareddy2606@gmail.com <raja@Raja.Dlink>
2018-06-14 18:04:21 +0530

    initial commit

---
+++ CogconsPods/CogconsPods/Cogcons.swift
@@ -0,0 +1,18 @@
+//
+//  Cogcons.swift
+//  CogconsPods
+//
+//  Created by Raja on 14/06/18.
+//  Copyright © 2018 Cogcons. All rights reserved.
+//
+
+import Foundation
+
+public class Cogcons {
+    
+    fileprivate init() {}
+    
+    public func doSomething() -> String {
+        return "You did Something"
+    }
+}
---
+++ CogconsPods/CogconsPods/CogconsPods.h
@@ -0,0 +1,19 @@
+//
+//  CogconsPods.h
+//  CogconsPods
+//
+//  Created by Raja on 14/06/18.
+//  Copyright © 2018 Cogcons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CogconsPods.
+FOUNDATION_EXPORT double CogconsPodsVersionNumber;
+
+//! Project version string for CogconsPods.
+FOUNDATION_EXPORT const unsigned char CogconsPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CogconsPods/PublicHeader.h>
+
+
